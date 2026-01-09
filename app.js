29108b74b7c645e13669aced95db4bd14dc8a502
Cole Detrick <153991478+coledetrick@users.noreply.github.com>
2026-01-09 16:10:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World\n");
+}).listen(port, "0.0.0.0", () => {
+  console.log(`Listening on ${port}`);
+});
+
